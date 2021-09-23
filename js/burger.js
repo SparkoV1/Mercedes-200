fbcdabf6828dd1ac1d890098d7e0601966db9d13
SparkoV1 <79722860+SparkoV1@users.noreply.github.com>
2021-09-23 23:17:40 +0200

    09/23/2021_add_burger

---
+++ js/burger.js
@@ -0,0 +1,16 @@
+const burger = document.querySelector('.humburger-menu')
+const menuElem = document.querySelector('.menu')
+const menuLinks = document.querySelectorAll('.menu-list__link')
+
+const toggleMenu = () => {
+    menuElem.classList.toggle('menu-active')
+    burger.classList.toggle('humburger-menu-active')
+}
+
+burger.addEventListener('click', () => toggleMenu())
+
+menuLinks.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        toggleMenu()
+    })
+})
