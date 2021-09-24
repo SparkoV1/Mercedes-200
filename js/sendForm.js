14095f7536a82dbe50a5fcef33fe0e4ece657cb8
SparkoV1 <79722860+SparkoV1@users.noreply.github.com>
2021-09-24 22:27:31 +0200

    09/24/2021_done

---
+++ js/sendForm.js
@@ -0,0 +1,43 @@
+const form = document.querySelector('.form-test-drive')
+const formURL = 'https://jsonplaceholder.typicode.com/posts'
+
+
+
+form.addEventListener('submit', (e) => {
+    let data = {}
+    let emptyField = false
+
+    e.preventDefault()
+
+    for (let {name, value} of form.elements) {
+        if (name) {
+            value = value.trim();
+            data[name] = value
+            if (value === '') emptyField = true
+        }
+    }
+    if (emptyField) {
+        alert('Заполнены не все поля формы! ')
+    } else {
+        fetch(formURL, {
+            method: 'POST',
+            body: JSON.stringify(data),
+        })
+            .then(response => {
+                if (response.status === 200 || response.status === 201) {
+                    return response.json()
+                } else {
+                    throw new Error(response.message)
+                }
+            })
+            .then(() => {
+                alert('Данные успешно сохранены!')
+                form.reset()
+            })
+            .catch(error => {
+                alert('Произошла ошибка, статус ' + error.message)
+            })
+    }
+
+
+})
--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,12 +1,15 @@
 const linksHeader = document.querySelectorAll('.menu-list__link')
 const mainScroll = document.querySelector('.main__scroll')
-const newArray = [...linksHeader, mainScroll]
+const logoImg = document.querySelector('.logo__img')
+const mainBtn = document.querySelector('.main__button')
+
+const newArray = [...linksHeader, mainScroll, logoImg, mainBtn]
 
 newArray.forEach(link => {
     link.addEventListener('click', (e) => {
         e.preventDefault()
 
-        const ID = e.target.getAttribute('href').substr(1)
+        const ID = e.target.closest("A").getAttribute('href').substr(1)
 
         document.getElementById(ID).scrollIntoView({
             behavior: 'smooth',
